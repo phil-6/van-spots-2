d816154398ade91ecb7e96de9e36e821395f80b1
Phil Reynolds <gpg@purpleriver.dev>
2025-02-19 18:25:48 +0000

    Get Map working again

---
+++ app/javascript/controllers/map_controller.js
@@ -0,0 +1,233 @@
+// app/javascript/controllers/map_controller.js
+import { Controller } from "@hotwired/stimulus";
+import { MarkerClusterer } from "@googlemaps/markerclusterer";
+
+export default class extends Controller {
+    static targets = ["map"];
+
+    connect() {
+        this.map = null;
+        this.markers = [];
+        this.markerCluster = null;
+        this.userPos = null;
+        this.infoWindow = null;
+        this.newMarker = null;
+        this.newMarkerInfoWindow = null;
+        this.icons = JSON.parse(this.mapTarget.dataset.mapIcons);
+
+        this.loadGoogleMapsAPI();
+    }
+
+    loadGoogleMapsAPI() {
+        if (typeof google === "undefined") {
+            const script = document.createElement("script");
+            script.src = `https://maps.googleapis.com/maps/api/js?key=<%= ENV['MAPS_API_KEY'] %>&callback=initMap`;
+            script.async = true;
+            script.defer = true;
+            window.initMap = this.initMap.bind(this);
+            document.head.appendChild(script);
+        } else {
+            this.initMap();
+        }
+    }
+
+    initMap() {
+        console.log('initMap() start')
+        this.map = new google.maps.Map(this.mapTarget, {
+            center: { lat: 51.6, lng: -4.15 },
+            zoom: 12,
+            mapTypeControl: true,
+            mapTypeControlOptions: {
+                position: google.maps.ControlPosition.LEFT_BOTTOM,
+            },
+            streetViewControl: false,
+            fullscreenControl: false,
+            scaleControl: true,
+            mapTypeId: "terrain",
+            mapId: "a2b97596989ec0c3"
+        });
+
+        this.infoWindow = new google.maps.InfoWindow();
+        this.newMarkerInfoWindow = new google.maps.InfoWindow({ map: this.map });
+
+        this.markerCluster = new MarkerClusterer({
+            map: this.map,
+            markers: []
+        });
+
+        fetch("/api/spots")
+            .then((response) => response.json())
+            .then((data) => {
+                data.forEach((item) => {
+                    const position = new google.maps.LatLng(item.latitude, item.longitude);
+                    const marker = new google.maps.Marker({
+                        map: this.map,
+                        position: position,
+                        id: item.id,
+                        name: item.name,
+                        description: item.description,
+                        average_rating: item.average_rating,
+                        type: item.spot_type,
+                        icon: this.icons[item.spot_type],
+                        animation: google.maps.Animation.DROP,
+                    });
+
+                    this.markers.push(marker);
+                    this.markerCluster.addMarker(marker);
+
+                    marker.addListener("click", () => {
+                        this.populateInfoWindow(marker, this.infoWindow);
+                    });
+                });
+            });
+
+        const mapFilterControlsDiv = document.createElement("div");
+        this.MapFilterControls(mapFilterControlsDiv, this.map);
+        mapFilterControlsDiv.index = 1;
+        this.map.controls[google.maps.ControlPosition.LEFT_CENTER].push(mapFilterControlsDiv);
+
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    this.userPos = {
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude,
+                    };
+                    const userMarker = new google.maps.Marker({
+                        position: this.userPos,
+                        animation: google.maps.Animation.DROP,
+                        icon: this.icons["user"],
+                        map: this.map,
+                    });
+                    const userInfoWindow = new google.maps.InfoWindow({ map: this.map });
+                    userInfoWindow.setContent(
+                        `<div class="text-center"><strong>You are here.</strong><br />${this.userPos.lat}, ${this.userPos.lng}</div>`
+                    );
+                    userMarker.addListener("click", () => {
+                        userInfoWindow.open(this.map, userMarker);
+                    });
+                    this.map.setCenter(this.userPos);
+                },
+                () => {
+                    this.handleLocationError(true, this.infoWindow, this.map.getCenter());
+                }
+            );
+        } else {
+            this.handleLocationError(false, this.infoWindow, this.map.getCenter());
+        }
+
+        google.maps.event.addListener(this.map, "click", (event) => {
+            this.placeNewMarker(event.latLng);
+        });
+    }
+
+    handleLocationError(browserHasGeolocation, infoWindow, userPos) {
+        infoWindow.setPosition(userPos);
+        infoWindow.setContent(
+            browserHasGeolocation
+                ? "Error: The Geolocation service failed."
+                : "Error: Your browser doesn't support geolocation."
+        );
+    }
+
+    populateInfoWindow(marker, infowindow) {
+        if (infowindow.marker !== marker) {
+            const markerTypeMap = {
+                free_spot: "Free Spot",
+                paid_spot: "Paid Spot",
+                campsite: "Campsite",
+                mtb_spot: "MTB Spot",
+                climbing_spot: "Climbing Spot",
+                kayaking_spot: "Kayaking Spot",
+                surf_spot: "Surf Spot",
+                bad_spot: "Bad Spot",
+            };
+            const markerType = markerTypeMap[marker.type] || "Unknown Spot Type";
+            const infoWindowContent = `
+        <div id="content">
+          <h2 id="infoWindowHeading" class="infoWindowHeading">
+            <a href="/spots/${marker.id}">${marker.name}</a>
+          </h2>
+          <div id="infoWindowContent" class="infoWindowContent">
+            <h6>${markerType} - ${marker.average_rating}/10</h6>
+            <p>${marker.description}</p>
+            <p>
+              <a class="btn btn-sm btn-secondary" href="https://www.google.com/maps/dir/?api=1&destination=${marker.position.lat()},${marker.position.lng()}">Directions</a>
+              <a class="btn btn-sm btn-info" href="/spots/${marker.id}">More Info</a>
+            </p>
+          </div>
+        </div>`;
+            infowindow.marker = marker;
+            infowindow.setContent(infoWindowContent);
+            infowindow.open(this.map, marker);
+            infowindow.addListener("closeclick", () => {
+                infowindow.marker = null;
+            });
+        }
+    }
+
+    placeNewMarker(location) {
+        if (this.newMarker) {
+            this.newMarker.setPosition(location);
+        } else {
+            this.newMarker = new google.maps.Marker({
+                position: location,
+                icon: "/assets/map-marker-lighter-ink.png",
+                map: this.map,
+            });
+        }
+        const newMarkerInfoWindowContent = `
+      <div id="content">
+        <a href="/spots/new?latitude=${location.lat()}&longitude=${location.lng()}"><strong> Add Spot Here </strong></a>
+        <br />Lat: ${location.lat()}, <br />Lng: ${location.lng()}
+      </div>`;
+        this.newMarkerInfoWindow.setContent(newMarkerInfoWindowContent);
+        this.newMarkerInfoWindow.open(this.map, this.newMarker);
+        this.newMarker.addListener("click", () => {
+            this.newMarkerInfoWindow.open(this.map, this.newMarker);
+        });
+    }
+
+    showListings() {
+        const bounds = new google.maps.LatLngBounds();
+        this.markers.forEach((marker) => {
+            marker.setMap(this.map);
+            this.markerCluster.addMarker(marker);
+            bounds.extend(marker.position);
+        });
+        this.map.fitBounds(bounds);
+        this.markerCluster.repaint();
+    }
+
+    hideListings() {
+        this.markers.forEach((marker) => {
+            marker.setMap(null);
+            this.markerCluster.removeMarker(marker);
+        });
+    }
+
+    MapFilterControls(controlDiv, map) {
+        const showAllUI = document.createElement("div");
+        showAllUI.id = "showAllUI";
+        showAllUI.title = "Show all listings";
+        controlDiv.appendChild(showAllUI);
+
+        const showAllText = document.createElement("div");
+        showAllText.id = "showAllText";
+        showAllText.innerHTML = "Show All";
+        showAllUI.appendChild(showAllText);
+
+        const hideAllUI = document.createElement("div");
+        hideAllUI.id = "hideAllUI";
+        hideAllUI.title = "Hide all listings";
+        controlDiv.appendChild(hideAllUI);
+
+        const hideAllText = document.createElement("div");
+        hideAllText.id = "hideAllText";
+        hideAllText.innerHTML = "Hide All";
+        hideAllUI.appendChild(hideAllText);
+
+        showAllUI.addEventListener("click", this.showListings.bind(this));
+        hideAllUI.addEventListener("click", this.hideListings.bind(this));
+    }
+}
--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,8 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
+
+pin "@googlemaps/markerclusterer", to: "@googlemaps--markerclusterer.js" # @2.5.3
+pin "fast-deep-equal" # @3.1.3
+pin "kdbush", to: "kdbush.js" # @4.0.2
+pin "supercluster" # @8.0.1
